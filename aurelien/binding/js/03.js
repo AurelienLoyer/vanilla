3472ac17d2aaf6d0edc8258923e8fe8e830089ab
Aurélien Loyer <aur.loy@gmail.com>
2018-05-10 17:32:06 +0200

    feat: first version of templating without watch state change

--- aurelien/binding/js/03.js
+++ aurelien/binding/js/03.js
@@ -1,20 +1,59 @@
-class app {
-  constructor(app, state) {
-    this.app = app;
-    this.state = state;
-  }
+var App = function(app, state) {
+  const regex = /{{2}(.*?)\}{2}/gis;
+  const stateElements = {};
 
-  bind() {
-    const elements = this.app.querySelectorAll("*");
+  function bind() {
+    const elements = app.querySelectorAll("*");
 
     elements.forEach(element => {
       // NodeList collection
-      element.childNodes.forEach(childnode => {});
+      element.childNodes.forEach(childnode => {
+        let m;
+
+        while ((m = regex.exec(childnode.nodeValue)) !== null) {
+          // This is necessary to avoid infinite loops with zero-width matches
+          if (m.index === regex.lastIndex) {
+            regex.lastIndex++;
+          }
+
+          let fullMatch;
+          let stateKey;
+
+          // The result can be accessed through the `m`-variable.
+          m.forEach((match, groupIndex) => {
+            if (groupIndex === 0) {
+              fullMatch = match;
+            } else {
+              stateKey = match.trim();
+            }
+          });
+
+          if (!Array.isArray(stateElements[stateKey]))
+            stateElements[stateKey] = [];
+
+          stateElements[stateKey].push({
+            node: childnode,
+            fullMatch
+          });
+        }
+
+        Object.keys(stateElements).forEach(key => {
+          stateElements[key].forEach(el => {
+            updateNode(el.node, el.fullMatch, key);
+          });
+        });
+      });
     });
   }
-}
 
-var toto = [];
+  function updateNode(node, fullMatch, key) {
+    node.nodeValue = node.nodeValue.replace(fullMatch, state[key]);
+  }
+
+  return {
+    bind
+  };
+};
 
 const state = {
   firstname: "Aurélien",
@@ -23,4 +62,4 @@ const state = {
 };
 
 const root = document.querySelector(".app");
-const myapp = new app(root, state).bind();
+const myapp = new App(root, state).bind();
