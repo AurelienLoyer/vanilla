5b3b86048a016219231fb716aaddc909fb4b8582
Aurélien Loyer <aur.loy@gmail.com>
2018-05-31 00:52:50 +0200

    feat(templating): add first working version of templating using proxy 🎉🎉🎉

--- aurelien/binding/js/03.js
+++ aurelien/binding/js/03.js
@@ -1,3 +1,7 @@
+const regex = /{{2}(.*?)\}{2}/gis;
+const attributesObservers = {};
+const stateElements = {};
+
 const handler = {
   apply: (target, that, args) => {},
   get: (target, attribute, proxy) => {
@@ -6,79 +10,76 @@ const handler = {
   set: (target, key, value, proxy) => {
     this._data[key] = value;
     stateElements[key].forEach(toto => {
-        console.log(toto)
-        updateNode(toto.node, toto.fullMatch, key);
-    })
+      updateNode(toto.node, toto.initialValue);
+    });
   }
 };
 
 function bind(data, app) {
-    this._data = data;
-    initObserver(app, data);
-    return new Proxy(data, handler);
+  this._data = data;
+  initObserver(app, data);
+  return new Proxy(data, handler);
 }
 
-function updateNode(node, fullMatch, key) {
-    console.log(this._data[key])
-    node.nodeValue = node.nodeValue.replace(fullMatch, this._data[key]);
+function updateNode(node, template) {
+  const toRename = extractMatchFromString(template);
+  toRename.forEach(el => {
+    template = template.replace(el.fullMatch, this._data[el.stateKey])
+    node.nodeValue = template;
+  });
 }
 
-/*function updateNode(attribute, value) {
-  const observers = attributesObservers[attribute];
-  if (observers) {
-    observers.forEach(observer => {
-      observer.element.setAttribute(observer.attribute, value);
+function extractMatchFromString(string) {
+  let m;
+  let matchs = [];
+  while ((m = regex.exec(string)) !== null) {
+
+    if (m.index === regex.lastIndex) {
+      regex.lastIndex++;
+    }
+
+    let fullMatch;
+    let stateKey;
+
+    m.forEach((match, groupIndex) => {
+      if (groupIndex === 0) {
+        fullMatch = match;
+      } else {
+        stateKey = match.trim();
+      }
     });
-  }
-}*/
 
-const attributesObservers = {};
-const stateElements = {};
+    matchs.push({
+      fullMatch,
+      stateKey
+    })
+  }
+
+  return matchs;
+}
+
+function addToStateElements(match, childnode) {
+  if (!Array.isArray(stateElements[match.stateKey]))
+        stateElements[match.stateKey] = [];
+
+  stateElements[match.stateKey].push({
+    node: childnode,
+    initialValue: childnode.nodeValue,
+  });
+}
 
 function initObserver(app, state) {
   const flatChildren = app.querySelectorAll("*");
-  const regex = /{{2}(.*?)\}{2}/gis;
 
   flatChildren.forEach(element => {
-    // NodeList collection
+
     element.childNodes.forEach(childnode => {
-      let m;
 
-      while ((m = regex.exec(childnode.nodeValue)) !== null) {
-        // This is necessary to avoid infinite loops with zero-width matches
-        if (m.index === regex.lastIndex) {
-          regex.lastIndex++;
-        }
-
-        let fullMatch;
-        let stateKey;
-
-        // The result can be accessed through the `m`-variable.
-        m.forEach((match, groupIndex) => {
-          if (groupIndex === 0) {
-            fullMatch = match;
-          } else {
-            stateKey = match.trim();
-          }
-        });
-
-        if (!Array.isArray(stateElements[stateKey]))
-          stateElements[stateKey] = [];
-
-        stateElements[stateKey].push({
-          node: childnode,
-          initialValue: childnode.nodeValue,
-          fullMatch
-        });
-      }
-
-      // once all node find and regex extract i update all node by key
-
-      Object.keys(stateElements).forEach(key => {
-        stateElements[key].forEach(el => {
-          updateNode(el.node, el.fullMatch, key);
-        });
-      });
+      const toRename = extractMatchFromString(childnode.nodeValue);
+      
+      toRename.forEach(el => addToStateElements(el, childnode))
+      toRename.forEach(el => updateNode(childnode, childnode.nodeValue))
+        
     });
   });
 }
