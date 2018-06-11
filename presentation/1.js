cf2c74ad3d2b0927f92ab2d6808ff5f43c178916
Aurélien Loyer <aur.loy@gmail.com>
2018-06-11 23:21:06 +0200

    chore: refacto all html page and js organization 🦆

---
+++ presentation/1.js
@@ -0,0 +1,16 @@
+function bind(app, state) {
+    const bindPattern = 'z-bind:';
+    const bindSugarPattern = ':';
+    const attributeRegex = new RegExp(`\^${bindPattern}|${bindSugarPattern}`);
+    const flatChildren = app.querySelectorAll('*');
+    flatChildren.forEach((element) => {
+        
+        Object.values(element.attributes).forEach((attribute) => {
+            if (attributeRegex.test(attribute.localName) && attribute.specified) {
+                const attributeName = attribute.localName.replace(attributeRegex, '')
+                element.setAttribute(attributeName, state[attribute.value]);
+            }
+        })
+        
+    });
+}
