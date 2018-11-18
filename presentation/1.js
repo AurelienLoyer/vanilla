1cf990214f21cb1e4d3c5a963dbf22567b109582
Aurélien Loyer <aur.loy@gmail.com>
2018-11-18 08:39:56 -0500

    fix: clean code 😬

--- presentation/1.js
+++ presentation/1.js
@@ -1,8 +1,10 @@
 function apply(app, state) {
+
     const bindPattern = 'z-bind:';
     const bindSugarPattern = ':';
     const attributeRegex = new RegExp(`\^${bindPattern}|${bindSugarPattern}`);
     const flatChildren = app.querySelectorAll('*');
+    
     flatChildren.forEach((element) => {
         
         Object.values(element.attributes).forEach((attribute) => {
