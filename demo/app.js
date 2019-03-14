0440a4b5a631bd946d9ff27520bded6d5a05a5f7
Aurélien Loyer <aur.loy@gmail.com>
2019-03-14 14:14:29 -0400

    feat: add backup step2 interpolation

--- demo/app.js
+++ demo/app.js
@@ -14,6 +14,40 @@ function apply(app, state) {
             }
         });
 
+        element.childNodes.forEach(child => {
+            if (child instanceof Text) {
+                if(!child.initialValue) {
+                    child.initialValue = child.nodeValue;
+                }
+                let text = child.initialValue;
+
+                const brackets = extractBrackets(text);
+                brackets.forEach(bracket => {
+                    text = text.replace(bracket.fullMatch, state[bracket.stateKey]);
+                });
+                child.nodeValue = text;
+            }
+        })
+
     });
 
+}
+
+function extractBrackets(text) {
+        const bracketRegex = /{{2}(.*?)}{2}/gi;
+
+    let template;
+    const templates = [];
+
+    do {
+        template = bracketRegex.exec(text);
+        if (template) {
+            templates.push({
+                fullMatch: template[0],
+                stateKey: template[1].trim()
+            });
+        }
+    } while (template);
+
+    return templates;
 }
